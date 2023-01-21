bd7bfa27b35d1e83a3a71eec53779a1d7afd4188
IgrecL <ngowg5p3v@relay.firefox.com>
2023-01-21 18:07:39 +0100

    Essai de parsing du Json

--- src/Main.elm
+++ src/Main.elm
@@ -1,44 +1,20 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, pre)
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (..)
 import Http
-import Random
-
--- FUNCTIONS
-
-type Msg
-  = Roll
-  | NewFace Int
-
-RandomInt msg = 
-    case msg of
-        Roll ->
-            ( model
-            , Random.generate NewFace (Random.int 1 6)
-            )
-        NewFace newFace ->
-            ( Model newFace
-            , Cmd.none
-        )
-
-cutAt lst n =
-    if n > 0
-    then case lst of
-        [] -> []
-        (x :: xs) -> cutAt xs (n-1)
-    else case lst of
-        [] -> []
-        (x :: xs) -> (x :: xs)
-
-getAt lst n = 
-    List.head (cutAt lst n) 
+import Json.Decode exposing (..)
+import Json.Decode.Pipeline as JP
+import Debug
 
 
 
+-- UTILS
 
-
-
+clean : String -> String
+clean str = String.slice 1 (String.length str - 1) str
 
 
 
@@ -55,37 +31,42 @@ main =
     , view = view
     }
 
+
+
 -- MODEL
 
 type Model
   = Failure
   | Loading
-  | Success String
+  | Success (Result Error Word)
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Loading
-  , Http.get
-      { url = "../static/Words.txt"
-      , expect = Http.expectString GotText
-      }
-  )
+  (Loading, getRandomWord)
+
+
 
 -- UPDATE
 
 type Msg
-  = GotText (Result Http.Error (getAt (String.split " " String) 3))
+  = MorePlease
+  | GotWord (Result Http.Error String)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    GotText result ->
+    MorePlease ->
+      (Loading, getRandomWord)
+
+    GotWord result ->
       case result of
-        Ok fullText ->
-          (Success fullText, Cmd.none)
+        Ok def ->
+          (Success (decodeString wordDecoder (clean def)), Cmd.none)
 
         Err _ ->
           (Failure, Cmd.none)
+
+
 
 -- SUBSCRIPTIONS
 
@@ -93,16 +74,97 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 
+
+
 -- VIEW
 
 view : Model -> Html Msg
 view model =
+  div []
+    [ h2 [] [ text "Random Words" ]
+    , viewWord model
+    ]
+
+viewWord : Model -> Html Msg
+viewWord model =
   case model of
     Failure ->
-      text "I was unable to load your book."
+      div []
+        [ text "I could not load a random word for some reason. "
+        , button [ onClick MorePlease ] [ text "Try Again!" ]
+        ]
 
     Loading ->
       text "Loading..."
 
-    Success fullText ->
-      pre [] [ text fullText ]
+    Success def ->
+      div []
+        [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
+        , blockquote [] [ text def ]
+        , p [ style "text-align" "right" ]
+            [ text "— "
+            , cite [] [ ]
+            , text (" by ")
+            ]
+        ]
+
+-- HTTP
+
+getRandomWord : Cmd Msg
+getRandomWord =
+  Http.get
+    { url = "https://api.dictionaryapi.dev/api/v2/entries/en/anywhere"
+    , expect = Http.expectString GotWord
+    }
+
+
+
+-- JSON PARSING
+
+type alias Definitions =
+    { definition : String
+    , synonyms : List String
+    , antonyms : List String
+    , example : String
+    }
+
+type alias Meanings =
+    { partsOfSpeech : String
+    , definitions : List Definitions
+    }
+
+type alias Phonetics =
+    { text : String
+    , audio : String
+    }
+
+type alias Word =
+    { word : String
+    , phonetic : String
+    , phonetics : List Phonetics
+    , meanings : List Meanings
+    }
+
+definitionsDecoder =
+    succeed Definitions
+        |> JP.required "definition" string
+        |> JP.required "synonyms" (list string)
+        |> JP.required "antonyms" (list string)
+        |> JP.required "example" string
+
+meaningsDecoder =
+    succeed Meanings
+        |> JP.required "partOfSpeech" string
+        |> JP.required "definitions" (list definitionsDecoder)
+
+phoneticsDecoder =
+    succeed Phonetics
+        |> JP.required "text" string
+        |> JP.required "audio" string
+
+wordDecoder =
+    succeed Word
+        |> JP.required "word" string
+        |> JP.required "phonetic" string
+        |> JP.required "phonetics" (list phoneticsDecoder)
+        |> JP.required "meanings" (list meaningsDecoder)
